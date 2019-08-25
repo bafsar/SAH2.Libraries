b67c4b67364e4a2b2ce643c5bf16039ce1a4e268
Bilal AFŞAR <bafsar@users.noreply.github.com>
2019-08-25 17:33:52 +0300

    Update IconHelper.cs

--- SAH2.Utilities/IconHelper.cs
+++ SAH2.Utilities/IconHelper.cs
@@ -1,4 +1,4 @@
-﻿/***********************************************************************************************************
+/***********************************************************************************************************
  ***********************************************************************************************************
  ***********************************************************************************************************
  ***                                                                                                     ***
@@ -12,18 +12,21 @@
  ***********************************************************************************************************
  **********************************************************************************************************/
 
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
 namespace SAH2.Utilities
 {
-#if (NET472 || NET48)
-    using System.Drawing.Imaging;
-
     /// <summary>
-    /// Provides helper methods for imaging
+    ///     Provides helper methods for imaging
     /// </summary>
     public static class IconHelper
     {
         /// <summary>
-        /// Converts a PNG image to an icon (ico)
+        ///     Converts a PNG image to an icon (ico)
         /// </summary>
         /// <param name="input">The input stream</param>
         /// <param name="output">The output stream</param>
@@ -34,7 +37,7 @@ namespace SAH2.Utilities
         {
             try
             {
-                var inputBitmap = (Bitmap)Image.FromStream(input);
+                var inputBitmap = (Bitmap) Image.FromStream(input);
                 if (inputBitmap == null)
                     return false;
 
@@ -42,17 +45,17 @@ namespace SAH2.Utilities
                 if (preserveAspectRatio)
                 {
                     if (inputBitmap.Width > inputBitmap.Height)
-                        height = ((float)inputBitmap.Height / inputBitmap.Width) * size;
+                        height = (float) inputBitmap.Height / inputBitmap.Width * size;
                     else
-                        width = ((float)inputBitmap.Width / inputBitmap.Height) * size;
+                        width = (float) inputBitmap.Width / inputBitmap.Height * size;
                 }
 
-                var newBitmap = new Bitmap(inputBitmap, new Size((int)width, (int)height));
+                var newBitmap = new Bitmap(inputBitmap, new Size((int) width, (int) height));
                 if (newBitmap == null)
                     return false;
 
                 // save the resized png into a memory stream for future use
-                using (MemoryStream memoryStream = new MemoryStream())
+                using (var memoryStream = new MemoryStream())
                 {
                     newBitmap.Save(memoryStream, ImageFormat.Png);
 
@@ -61,38 +64,38 @@ namespace SAH2.Utilities
                         return false;
 
                     // 0-1 reserved, 0
-                    iconWriter.Write((byte)0);
-                    iconWriter.Write((byte)0);
+                    iconWriter.Write((byte) 0);
+                    iconWriter.Write((byte) 0);
 
                     // 2-3 image type, 1 = icon, 2 = cursor
-                    iconWriter.Write((short)1);
+                    iconWriter.Write((short) 1);
 
                     // 4-5 number of images
-                    iconWriter.Write((short)1);
+                    iconWriter.Write((short) 1);
 
                     // image entry 1
                     // 0 image width
-                    iconWriter.Write((byte)width);
+                    iconWriter.Write((byte) width);
                     // 1 image height
-                    iconWriter.Write((byte)height);
+                    iconWriter.Write((byte) height);
 
                     // 2 number of colors
-                    iconWriter.Write((byte)0);
+                    iconWriter.Write((byte) 0);
 
                     // 3 reserved
-                    iconWriter.Write((byte)0);
+                    iconWriter.Write((byte) 0);
 
                     // 4-5 color planes
-                    iconWriter.Write((short)0);
+                    iconWriter.Write((short) 0);
 
                     // 6-7 bits per pixel
-                    iconWriter.Write((short)32);
+                    iconWriter.Write((short) 32);
 
                     // 8-11 size of image data
-                    iconWriter.Write((int)memoryStream.Length);
+                    iconWriter.Write((int) memoryStream.Length);
 
                     // 12-15 offset of image data
-                    iconWriter.Write((int)(6 + 16));
+                    iconWriter.Write(6 + 16);
 
                     // write image data
                     // png data must contain the whole png data file
@@ -102,7 +105,6 @@ namespace SAH2.Utilities
                 }
 
                 return true;
-
             }
             catch (Exception ex)
             {
@@ -116,42 +118,38 @@ namespace SAH2.Utilities
         }
 
         /// <summary>
-        /// Converts a PNG image to an icon (ico)
+        ///     Converts a PNG image to an icon (ico)
         /// </summary>
         /// <param name="inputPath">The input path</param>
         /// <param name="outputPath">The output path</param>
         /// <param name="size">Needs to be a factor of 2 (16x16 px by default)</param>
         /// <param name="preserveAspectRatio">Preserve the aspect ratio</param>
         /// <returns>Wether or not the icon was succesfully generated</returns>
-        public static bool ConvertToIcon(string inputPath, string outputPath, int size = 16, bool preserveAspectRatio = false)
+        public static bool ConvertToIcon(string inputPath, string outputPath, int size = 16,
+            bool preserveAspectRatio = false)
         {
-            using (FileStream inputStream = new FileStream(inputPath, FileMode.Open))
-            using (FileStream outputStream = new FileStream(outputPath, FileMode.OpenOrCreate))
+            using (var inputStream = new FileStream(inputPath, FileMode.Open))
+            using (var outputStream = new FileStream(outputPath, FileMode.OpenOrCreate))
             {
                 return ConvertToIcon(inputStream, outputStream, size, preserveAspectRatio);
             }
         }
 
         /// <summary>
-        /// Converts a PNG image to an icon (ico)
+        ///     Converts a PNG image to an icon (ico)
         /// </summary>
         /// <param name="inputPath">Image object</param>
         /// <param name="preserveAspectRatio">Preserve the aspect ratio</param>
         /// <returns>ico byte array / null for error</returns>
         public static byte[] ConvertToIcon(Image image, bool preserveAspectRatio = false)
         {
-            MemoryStream inputStream = new MemoryStream();
+            var inputStream = new MemoryStream();
             image.Save(inputStream, ImageFormat.Png);
             inputStream.Seek(0, SeekOrigin.Begin);
-            MemoryStream outputStream = new MemoryStream();
-            int size = image.Size.Width;
-            if (!ConvertToIcon(inputStream, outputStream, size, preserveAspectRatio))
-            {
-                return null;
-            }
+            var outputStream = new MemoryStream();
+            var size = image.Size.Width;
+            if (!ConvertToIcon(inputStream, outputStream, size, preserveAspectRatio)) return null;
             return outputStream.ToArray();
         }
     }
-
-#endif
 }
