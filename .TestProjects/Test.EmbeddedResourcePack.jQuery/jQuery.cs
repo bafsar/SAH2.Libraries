aecb84710dd9538f39e9cf568a7dce07d92b7c7e
Bilal AFŞAR <bafsar@users.noreply.github.com>
2019-08-26 00:47:54 +0300

    Add files via upload

---
+++ .TestProjects/Test.EmbeddedResourcePack.jQuery/jQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Web.Mvc;
+using SAH2.Web.MVC.Utilities.EmbeddedResourcePack.Base;
+
+namespace Test.EmbeddedResourcePack
+{
+    public class jQuery : CustomEmbeddedBundle<jQuery>
+    {
+        public static MvcHtmlString JsOnLocal => AutoRender("Test.EmbeddedResourcePack/libs/js/jquery-3.4.1.min.js");
+        public static MvcHtmlString JsOnCDN => AutoRender("//code.jquery.com/jquery-3.4.1.min.js");
+    }
+}
